71050d868020284c45e4a09713ce14925cc5cdc9
Lutz Weigold <dev@lutzweigold.de>
2023-08-08 10:39:26 +0200

    Config: Add pinia dependency ⚙️: Can be used for state management

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: {enabled: true},
-    modules: [
-        '@nuxtjs/eslint-module',
-        '@nuxtjs/tailwindcss',
-        '@nuxt/image'
-    ]
-})
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/eslint-module', '@nuxtjs/tailwindcss', '@nuxt/image', '@pinia/nuxt'],
+});
