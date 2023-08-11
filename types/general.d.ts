245a2dd4317d964970418619286c61b54bf6d204
Lutz Weigold <dev@lutzweigold.de>
2023-08-11 18:21:49 +0200

    Config: Add general types ⚙️: Can be used for vue props

---
+++ types/general.d.ts
@@ -0,0 +1,8 @@
+type WrapperTag = 'div' | 'section' | 'header' | 'footer' | 'aside' | 'nav';
+type TextTag = 'h1' | 'h2' | 'h3' | 'h4' | 'p' | 'li' | 'span';
+
+type Variant = 'sm' | 'md' | 'lg';
+
+type Theme = 'light' | 'dark';
+
+export { TextTag, WrapperTag, Variant, Theme };
---
+++ types/index.d.ts
@@ -0,0 +1,3 @@
+import { TextTag, WrapperTag, Variant, Theme } from '@Types/general';
+
+export { TextTag, WrapperTag, Variant, Theme };
