469234da0030e893df8022acd82bff83e777a7a1
Lutz Weigold <dev@lutzweigold.de>
2023-08-11 18:19:30 +0200

    Feat: Add NavigationItem component

---
+++ components/atoms/NavigationItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <li class="flex items-baseline gap-2">
+    <span class="text-xl">0{{ props.prefix }}</span>
+    <NuxtLink :to="props.to">
+      <AtomsDisplayText class="pointer-events-auto cursor-pointer" html-tag="span">
+        <slot />
+      </AtomsDisplayText>
+    </NuxtLink>
+  </li>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  to: {
+    type: String,
+    required: true,
+  },
+  prefix: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
