ed1bc49f08ff7a88909cf9017171bcef14e785f3
Lutz Weigold <dev@lutzweigold.de>
2023-08-11 18:19:54 +0200

    Feat: Add Header component

---
+++ components/organisms/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <header class="grid h-screen w-full grid-cols-12 bg-gray-300">
+    <MoleculesNavigation class="col-span-full flex items-center xl:col-span-10 xl:col-start-3 xl:justify-end" />
+  </header>
+</template>
+
+<script lang="ts" setup></script>
