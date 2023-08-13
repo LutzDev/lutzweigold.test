b1b2cb8686de577db1a903bcb7505f9df5248c32
Lutz Weigold <dev@lutzweigold.de>
2023-08-13 13:17:16 +0200

    Feat: Add smallText component

---
+++ components/atoms/SmallText.vue
@@ -0,0 +1,18 @@
+<template>
+  <component :is="props.htmlTag" class="text-small">
+    <slot />
+  </component>
+</template>
+
+<script lang="ts" setup>
+import { PropType } from 'vue';
+import { TextTag } from '@Types';
+
+const props = defineProps({
+  htmlTag: {
+    type: String as PropType<TextTag>,
+    required: false,
+    default: 'p',
+  },
+});
+</script>
