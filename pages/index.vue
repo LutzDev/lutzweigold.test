490e3ca1e53bb009eb30a504c8a6ab8da86f643f
Lutz Weigold <dev@lutzweigold.de>
2023-08-13 13:27:28 +0200

    Feat: Add content to home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>Index</div>
+  <AtomsWrapper class="h-screen bg-purple-500" html-tag="section" @click="console.log('Section 1')">
+    <AtomsHeadlineText html-tag="h1">The quick brown fox jumps over the lazy dog</AtomsHeadlineText>
+    <AtomsHeadlineText html-tag="h2">The quick brown fox jumps over the lazy dog</AtomsHeadlineText>
+    <AtomsHeadlineText html-tag="h3">The quick brown fox jumps over the lazy dog</AtomsHeadlineText>
+    <AtomsHeadlineText html-tag="h4">The quick brown fox jumps over the lazy dog</AtomsHeadlineText>
+  </AtomsWrapper>
+  <AtomsWrapper class="h-screen bg-purple-500" html-tag="section" @click="console.log('Section 1')">
+    Section 2
+  </AtomsWrapper>
+  <AtomsWrapper class="h-screen bg-purple-500" html-tag="section" @click="console.log('Section 1')">
+    Section 3
+  </AtomsWrapper>
 </template>
 
 <script lang="ts" setup></script>
