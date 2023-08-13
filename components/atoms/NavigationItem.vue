e38968fb8d3e81f7339013c595666451fe9aa9cb
Lutz Weigold <dev@lutzweigold.de>
2023-08-13 13:19:25 +0200

    Fix: Text styles are now using predefined components

--- components/atoms/NavigationItem.vue
+++ components/atoms/NavigationItem.vue
@@ -1,6 +1,6 @@
 <template>
   <li class="flex items-baseline gap-2">
-    <span class="text-xl">0{{ props.prefix }}</span>
+    <AtomsBodyText html-tag="span">0{{ props.prefix }}</AtomsBodyText>
     <NuxtLink :to="props.to">
       <AtomsDisplayText class="pointer-events-auto cursor-pointer" html-tag="span">
         <slot />
--- components/organisms/Aside.vue
+++ components/organisms/Aside.vue
@@ -1,25 +1,25 @@
 <template>
   <aside class="grid-row-1 fixed left-0 right-0 top-0 z-10 grid h-[100dvh] w-full grid-cols-12 xl:grid-rows-2">
     <AtomsWrapper
-      class="pointer-events-auto col-span-full col-start-1 flex justify-between self-start xl:col-span-2 xl:block"
+      class="pointer-events-auto col-span-full col-start-1 flex justify-between self-start xl:col-span-2 xl:block xl:space-y-4"
     >
-      <div @click="console.log('Logo')">crafted by lutz.</div>
+      <AtomsBodyText @click="console.log('Logo')">crafted by lutz.</AtomsBodyText>
       <div>
-        <div>+49 152 26269766</div>
-        <div>info@lutzweigold.de</div>
+        <AtomsSmallText>+49 152 26269766</AtomsSmallText>
+        <AtomsSmallText>info@lutzweigold.de</AtomsSmallText>
       </div>
     </AtomsWrapper>
     <AtomsWrapper class="col-span-full col-start-1 row-start-2 self-end xl:col-span-2">
       <div
-        class="flex flex-row-reverse divide-x-2 divide-x-reverse divide-white border-2 xl:block xl:divide-x-0 xl:divide-y-2"
+        class="flex flex-row-reverse divide-x-2 divide-x-reverse divide-white border-2 border-white xl:block xl:divide-x-0 xl:divide-y-2"
       >
         <div class="flex w-1/2 justify-between px-2 py-1 text-white xl:w-full">
-          <span> Menu </span>
-          <span> [about] </span>
+          <AtomsBodyText html-tag="span">Menu</AtomsBodyText>
+          <AtomsBodyText html-tag="span">[about]</AtomsBodyText>
         </div>
         <div class="flex w-1/2 justify-between px-2 py-1 text-white xl:w-full">
-          <span> Language </span>
-          <span> [DE] </span>
+          <AtomsBodyText html-tag="span">Language</AtomsBodyText>
+          <AtomsBodyText html-tag="span">[DE]</AtomsBodyText>
         </div>
       </div>
     </AtomsWrapper>
