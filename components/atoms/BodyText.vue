2e8a0348d0921ba0c0f4e54fa8e864658ec005a6
Lutz Weigold <dev@lutzweigold.de>
2023-08-13 13:17:35 +0200

    Feat: Add bodyText component

---
+++ components/atoms/BodyText.vue
@@ -0,0 +1,18 @@
+<template>
+  <component :is="props.htmlTag" class="text-body">
+    <slot />
+  </component>
+</template>
+
+<script lang="ts" setup>
+import { PropType } from 'vue';
+import { TextTag } from '@Types';
+
+const props = defineProps({
+  htmlTag: {
+    type: String as PropType<TextTag>,
+    required: false,
+    default: 'p',
+  },
+});
+</script>
