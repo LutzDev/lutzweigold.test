5e8b738abbfc0eaef372487a6bbfee97adb001df
Lutz Weigold <dev@lutzweigold.de>
2023-08-13 13:17:25 +0200

    Feat: Add headlineText component

---
+++ components/atoms/HeadlineText.vue
@@ -0,0 +1,29 @@
+<template>
+  <component
+    :is="props.htmlTag"
+    :class="`${
+      props.htmlTag === 'h1'
+        ? 'first-headline'
+        : props.htmlTag === 'h2'
+        ? 'second-headline'
+        : props.htmlTag === 'h3'
+        ? 'third-headline'
+        : 'fourth-headline'
+    }`"
+  >
+    <slot />
+  </component>
+</template>
+
+<script lang="ts" setup>
+import { PropType } from 'vue';
+import { HeadlineTag } from '@Types';
+
+const props = defineProps({
+  htmlTag: {
+    type: String as PropType<HeadlineTag>,
+    required: true,
+    default: 'h1',
+  },
+});
+</script>
