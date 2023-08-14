784a84f22e680d0a4168a8cad90099c8bd61415f
Lutz Weigold <dev@lutzweigold.de>
2023-08-14 18:11:55 +0200

    Feat: Add FullScreenWrapper

---
+++ components/atoms/FullScreenWrapper.vue
@@ -0,0 +1,23 @@
+<template>
+  <AtomsWrapper :html-tag="props.htmlTag" class="flex min-h-screen flex-wrap content-between" variant="md">
+    <slot />
+  </AtomsWrapper>
+</template>
+
+<script lang="ts" setup>
+import { PropType } from 'vue/dist/vue';
+import { Variant, WrapperTag } from '@Types';
+
+const props = defineProps({
+  htmlTag: {
+    type: String as PropType<WrapperTag>,
+    required: false,
+    default: 'div',
+  },
+  variant: {
+    type: String as PropType<Variant>,
+    required: false,
+    default: 'md',
+  },
+});
+</script>
