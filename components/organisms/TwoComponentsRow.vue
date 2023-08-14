ca690fc3192f46ddd9423f50f0bbc7f3036193f2
Lutz Weigold <dev@lutzweigold.de>
2023-08-14 18:11:37 +0200

    Feat: Add TwoComponentsRow

---
+++ components/organisms/TwoComponentsRow.vue
@@ -0,0 +1,25 @@
+<template>
+  <AtomsGrid :class="`h-full w-full`" :cols="10" :theme="props.theme">
+    <AtomsGrid :cols="5" class="col-span-full w-full md:col-span-5">
+      <slot :theme="props.theme" name="leftCol" />
+    </AtomsGrid>
+    <AtomsGrid :cols="5" class="col-span-full w-full md:col-span-5">
+      <slot :theme="props.theme" name="rightCol" />
+    </AtomsGrid>
+  </AtomsGrid>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  fullScreen: {
+    type: Boolean,
+    default: false,
+    required: false,
+  },
+  theme: {
+    type: String,
+    default: 'bright',
+    required: false,
+  },
+});
+</script>
