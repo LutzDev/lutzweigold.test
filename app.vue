87a036f10a84ee98f51ec61202ee14fa91978631
Lutz Weigold <dev@lutzweigold.de>
2023-08-14 18:12:20 +0200

    Feat: Add smooth-scroll for mobile devices

--- app.vue
+++ app.vue
@@ -15,7 +15,7 @@ onMounted(() => {
   ScrollSmoother.create({
     smooth: 2, // how long (in seconds) it takes to "catch up" to native scroll position
     effects: true, // looks for data-speed and data-lag attributes on elements
-    smoothTouch: 0, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+    smoothTouch: 0.5, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
   });
 });
 </script>
