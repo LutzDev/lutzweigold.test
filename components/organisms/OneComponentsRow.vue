d9b6a05f68935c3b7e371e19afb55e01445e3f67
Lutz Weigold <dev@lutzweigold.de>
2023-08-14 18:11:45 +0200

    Feat: Add OneComponentsRow

---
+++ components/organisms/OneComponentsRow.vue
@@ -0,0 +1,20 @@
+<template>
+  <AtomsGrid :class="`h-full w-full`" :cols="10" :theme="props.theme">
+    <slot :theme="props.theme" />
+  </AtomsGrid>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  fullScreen: {
+    type: Boolean,
+    default: false,
+    required: false,
+  },
+  theme: {
+    type: String,
+    default: 'bright',
+    required: false,
+  },
+});
+</script>
