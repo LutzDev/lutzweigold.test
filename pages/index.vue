f4ab94a3ca4a62cf30fa046ec4e08eca409eb152
Lutz Weigold <dev@lutzweigold.de>
2023-08-16 20:03:34 +0200

    Feat: Update home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,52 +1,36 @@
 <template>
-  <div ref="wrapper">
-    <AtomsFullScreenWrapper html-tag="section">
-      <OrganismsOneComponentsRow>
-        <AtomsHeadlineText html-tag="h1" class="col-span-8"
-          >I'm a creative software developer from germany
-        </AtomsHeadlineText>
-      </OrganismsOneComponentsRow>
-      <OrganismsTwoComponentsRow>
-        <template #leftCol>
-          <div
-            class="col-span-full col-start-1 row-span-1 row-start-1 overflow-hidden xs:col-span-3 xs:col-start-2 md:col-span-full md:col-start-1 lg:col-span-4 lg:col-start-2"
-          >
-            <div ref="firstImg" class="scale-105">
+  <AtomsWrapper is-full-screen html-tag="section" y-padding="lg">
+    <template #default="{ isFullScreen }">
+      <AtomsFlex :is-full-screen="isFullScreen" wrap y-gap="lg">
+        <OrganismsOneComponentsRow>
+          <AtomsHeadline html-tag="h1" class="col-span-full lg:col-span-6">
+            I'm a creative developer based in germany
+          </AtomsHeadline>
+        </OrganismsOneComponentsRow>
+        <OrganismsTwoComponentsRow>
+          <template #leftCol>
+            <div
+              class="col-span-3 col-start-2 overflow-hidden md:col-span-full md:col-start-1 lg:col-span-4 lg:col-start-2"
+            >
               <nuxt-picture
                 fit="cover"
-                src="/images/portrait-1.jpg"
+                src="/images/portrait-2.jpg"
                 :img-attrs="{
-                  class: 'img object-cover w-full h-60',
+                  class: 'img object-cover w-full h-30 xs:h-40 sm:h-50 lg:h-55 xl:h-60 2xl:h-75 3xl:h-90 4xl:h-100',
                 }"
               />
             </div>
-          </div>
-          <div
-            ref="secondImg"
-            class="col-span-full col-start-1 row-span-1 row-start-1 overflow-hidden xs:col-span-3 xs:col-start-2 md:col-span-full md:col-start-1 lg:col-span-4 lg:col-start-2"
-            :style="{ clipPath: 'polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)' }"
-          >
-            <nuxt-picture
-              fit="cover"
-              src="/images/portrait-1.jpg"
-              :img-attrs="{
-                class: 'img object-cover w-full h-60',
-              }"
-            />
-          </div>
-        </template>
-        <template #rightCol>
-          <AtomsBodyText
-            html-tag="p"
-            class="col-span-4 col-start-2 self-end xs:col-span-3 xs:col-start-2 md:col-span-full lg:col-span-3 lg:col-start-3 lg:justify-self-end"
-            >The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown
-            fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.
-          </AtomsBodyText>
-        </template>
-      </OrganismsTwoComponentsRow>
-    </AtomsFullScreenWrapper>
-  </div>
-
+          </template>
+          <template #rightCol>
+            <AtomsBodyText html-tag="p" class="col-span-full col-start-1 self-end lg:col-span-3 lg:col-start-3"
+              >Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+              industry's standard dummy text ever since the 1500s, when an unknown printer..</AtomsBodyText
+            >
+          </template>
+        </OrganismsTwoComponentsRow>
+      </AtomsFlex>
+    </template>
+  </AtomsWrapper>
   <AtomsWrapper class="h-screen bg-purple-500" html-tag="section" @click="console.log('Section 1')">
     Section 2
   </AtomsWrapper>
@@ -56,16 +40,17 @@
 </template>
 
 <script lang="ts" setup>
-const { $gsap: gsap, $ScrollTrigger: ScrollTrigger } = useNuxtApp();
+/* const { $gsap: gsap, $ScrollTrigger: ScrollTrigger } = useNuxtApp();
 
 const firstImg = ref<HTMLDivElement | null>(null);
 const secondImg = ref<HTMLDivElement | null>(null);
+const imgWrapper = ref<HTMLDivElement | null>(null);
 const wrapper = ref();
 onMounted(() => {
   const tl = gsap.timeline();
 
   tl.to(
-    secondImg.value,
+    imgWrapper.value,
     {
       clipPath: 'polygon(0 0%, 100% 0%, 100% 100%, 0% 100%)',
     },
@@ -75,7 +60,15 @@ onMounted(() => {
   tl.to(
     firstImg.value,
     {
-      y: 20,
+      y: 25,
+    },
+    0
+  );
+
+  tl.from(
+    secondImg.value,
+    {
+      y: 25,
     },
     0
   );
@@ -84,14 +77,9 @@ onMounted(() => {
     trigger: wrapper.value,
     start: 'top top',
     end: 'bottom top',
-    markers: true,
     pin: true,
     scrub: true,
     animation: tl,
-    onToggle: (self) => console.log('toggled, isActive:', self.isActive),
-    onUpdate: (self) => {
-      console.log('progress:', self.progress.toFixed(3), 'direction:', self.direction, 'velocity', self.getVelocity());
-    },
   });
-});
+}); */
 </script>
