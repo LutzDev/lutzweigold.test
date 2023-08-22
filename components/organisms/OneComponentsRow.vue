0b7799953253c058bf2a62aeb4fb842af6bc79fc
Lutz Weigold <dev@lutzweigold.de>
2023-08-22 22:51:29 +0200

    Feat: Implement new interfaces for OneComponentsRow.vue

--- components/organisms/OneComponentsRow.vue
+++ components/organisms/OneComponentsRow.vue
@@ -1,15 +1,45 @@
 <template>
-  <AtomsGrid :class="`w-full`" :cols="10" :theme="props.theme">
+  <AtomsGrid
+    :class="`w-full`"
+    :cols="props.cols"
+    :rows="props.rows"
+    :y-gap="props.yGap"
+    :x-gap="props.xGap"
+    :theme="props.theme"
+  >
     <slot :theme="props.theme" />
   </AtomsGrid>
 </template>
 
 <script lang="ts" setup>
+import { PropType } from 'vue/dist/vue';
+import { Grid, Variant } from '@Types';
+
 const props = defineProps({
   fullScreen: {
     type: Boolean,
     default: false,
     required: false,
+  },
+  cols: {
+    type: [Number, String] as PropType<Grid>,
+    default: '10',
+    required: false,
+  },
+  rows: {
+    type: [Number, String] as PropType<Grid>,
+    default: 'none',
+    required: false,
+  },
+  yGap: {
+    type: String as PropType<Variant>,
+    required: false,
+    default: 'md',
+  },
+  xGap: {
+    type: String as PropType<Variant>,
+    required: false,
+    default: 'md',
   },
   theme: {
     type: String,
