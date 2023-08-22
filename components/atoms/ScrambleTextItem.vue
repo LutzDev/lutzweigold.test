99939b455651d0c9ab7eb5e066cafdcd3039b579
Lutz Weigold <dev@lutzweigold.de>
2023-08-22 22:54:14 +0200

    Feat: Add component for scrambled text sections

---
+++ components/atoms/ScrambleTextItem.vue
@@ -0,0 +1,73 @@
+<template>
+  <div ref="target" class="text-body">
+    {{ props.title }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ScrambleTextItem } from '@Types';
+
+const { $gsap: gsap, $Power4: Power4, $SplitText: SplitText } = useNuxtApp();
+const target = ref<HTMLDivElement | null>(null);
+
+let tl: gsap.core.Timeline;
+let ctx: gsap.Context;
+let shuffleCharArray: Array<any>;
+
+const emit = defineEmits<{
+  (e: 'timeline', value: ScrambleTextItem): void;
+}>();
+
+onMounted(() => {
+  ctx = gsap.context(() => {
+    const text = new SplitText(target.value, { types: 'chars', charsClass: 'opacity-0' });
+    shuffleCharArray = useShuffle(Array.from(text.chars));
+    tl = gsap.timeline({
+      repeatDelay: 1,
+      defaults: { ease: Power4.easeInOut, duration: 1 },
+    });
+
+    tl.set(text.chars, {
+      opacity: '1',
+      stagger: 0.05,
+    });
+    tl.set(
+      shuffleCharArray,
+      {
+        opacity: '0',
+        stagger: 0.05,
+      },
+      '+=0.5'
+    );
+    tl.set(
+      text.chars,
+      {
+        opacity: '1',
+        stagger: 0.05,
+      },
+      '-=0'
+    );
+    tl.set(text.chars, {
+      opacity: '0',
+      stagger: 0.05,
+    });
+  });
+  emit('timeline', { tl, delay: Number(props.delay) / 4 });
+});
+
+onUnmounted(() => {
+  ctx.revert();
+});
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  delay: {
+    type: Number,
+    default: 0,
+    required: false,
+  },
+});
+</script>
---
+++ components/molecules/ScrambleText.vue
@@ -0,0 +1,51 @@
+<template>
+  <div ref="wrapper" @click="tl?.play()">
+    <div v-for="(item, key) in props.rows" :key="key">
+      <AtomsScrambleTextItem v-if="props.title" :title="props.title" :delay="key" @timeline="handleEmit" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ScrambleTextItem } from '@Types';
+
+const { $gsap: gsap, $ScrollTrigger: ScrollTrigger } = useNuxtApp();
+const wrapper = ref<HTMLDivElement | null>(null);
+
+const tl = shallowRef<gsap.core.Timeline>();
+let ctx: gsap.Context;
+
+const handleEmit = (value: ScrambleTextItem) => {
+  tl.value?.add(value.tl, value.delay);
+};
+
+onBeforeMount(() => {
+  tl.value = gsap.timeline({ paused: true, repeat: -1, repeatDelay: 1, smoothChildTiming: true });
+});
+onMounted(() => {
+  ctx = gsap.context(() => {
+    ScrollTrigger.create({
+      trigger: wrapper.value,
+      toggleActions: 'play pause resume pause',
+      animation: tl.value?.seek(gsap.utils.random(1, 3)),
+      refreshPriority: 1,
+    });
+  });
+});
+
+onUnmounted(() => {
+  ctx.revert();
+});
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  rows: {
+    type: Number,
+    default: 1,
+    required: false,
+  },
+});
+</script>
