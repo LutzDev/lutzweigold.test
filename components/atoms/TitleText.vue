3935c436e2093246caec5acf30c9a79454b36dce
Lutz Weigold <dev@lutzweigold.de>
2023-08-22 22:53:57 +0200

    Feat: Add component for displaying titles

---
+++ components/atoms/TitleText.vue
@@ -0,0 +1,19 @@
+<template>
+  <component :is="props.htmlTag" class="text-title font-light">
+    <slot />
+  </component>
+</template>
+
+<script lang="ts" setup>
+import { PropType } from 'vue';
+import { TextTag } from '@Types';
+
+const props = defineProps({
+  htmlTag: {
+    type: String as PropType<TextTag>,
+    required: false,
+    default: 'span',
+  },
+});
+</script>
+p
