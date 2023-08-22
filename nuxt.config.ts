6e04f36ec48de95a9e4bc436eb0424749b97fa3a
Lutz Weigold <dev@lutzweigold.de>
2023-08-22 22:50:03 +0200

    Config: Add vueuse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/eslint-module', '@nuxtjs/tailwindcss', '@nuxt/image', '@pinia/nuxt'],
+  modules: ['@nuxtjs/eslint-module', '@nuxtjs/tailwindcss', '@vueuse/nuxt', '@nuxt/image', 'nuxt-icon', '@pinia/nuxt'],
   css: ['@/assets/css/main.css'],
   alias: {
     '@Types': './types',
