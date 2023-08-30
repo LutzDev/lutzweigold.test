83e8a8518c405df49c136a875c9010e1ee3133e2
Lutz Weigold <dev@lutzweigold.de>
2023-08-30 17:30:49 +0200

    Feat: Add different variants of title

--- components/atoms/TitleText.vue
+++ components/atoms/TitleText.vue
@@ -1,18 +1,35 @@
 <template>
-  <component :is="props.htmlTag" class="text-title font-light">
+  <component
+    :is="props.htmlTag"
+    :class="`${
+      props.size === 'sm'
+        ? 'text-title-sm font-normal'
+        : props.size === 'md'
+        ? 'text-title-md font-light'
+        : props.size === 'lg'
+        ? 'text-title-lg font-light'
+        : ''
+    } `"
+  >
     <slot />
   </component>
 </template>
 
 <script lang="ts" setup>
 import { PropType } from 'vue';
-import { TextTag } from '@Types';
+import { TextTag, Variant } from '@Types';
 
 const props = defineProps({
   htmlTag: {
     type: String as PropType<TextTag>,
     required: false,
     default: 'span',
+  },
+
+  size: {
+    type: String as PropType<Variant>,
+    required: false,
+    default: 'md',
   },
 });
 </script>
