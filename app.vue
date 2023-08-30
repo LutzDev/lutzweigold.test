e01222ee7920b459f78655107f10604c405e2ec0
Lutz Weigold <dev@lutzweigold.de>
2023-08-30 17:32:24 +0200

    Refactor: Remove comments

--- app.vue
+++ app.vue
@@ -13,9 +13,9 @@
 const { $ScrollSmoother: ScrollSmoother } = useNuxtApp();
 onMounted(() => {
   ScrollSmoother.create({
-    smooth: 2, // how long (in seconds) it takes to "catch up" to native scroll position
-    effects: true, // looks for data-speed and data-lag attributes on elements
-    smoothTouch: 0.5, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+    smooth: 2,
+    effects: true,
+    smoothTouch: 0.5,
   });
 });
 </script>
