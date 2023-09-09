61d4a3e9fa53e3f4acbbf9581cf453020f9d1771
Lutz Weigold <dev@lutzweigold.de>
2023-09-09 18:39:37 +0200

    Feat: Add skill page

---
+++ pages/skill.vue
@@ -0,0 +1,38 @@
+<template>
+  <OrganismsSvgVisual />
+  <AtomsWrapper html-tag="section" y-padding="xl" class="bg-purple-400">
+    <template #default>
+      <OrganismsOneComponentsRow y-gap="none">
+        <MoleculesList :list="list" :title="{ name: 'Backend Development', reverse: true }" />
+      </OrganismsOneComponentsRow>
+    </template>
+  </AtomsWrapper>
+  <AtomsWrapper html-tag="section" y-padding="lg" class="bg-purple-400">
+    <template #default>
+      <OrganismsOneComponentsRow y-gap="none">
+        <MoleculesList :list="list" :title="{ name: 'Frontend Development' }" />
+      </OrganismsOneComponentsRow>
+    </template>
+  </AtomsWrapper>
+</template>
+
+<script setup lang="ts">
+import { ListItem } from '@Types';
+
+const list: Array<ListItem> = [
+  {
+    title: 'Contact details',
+    entries: [
+      { name: 'info@lutzweigold.de', to: 'mailto:info@lutzweigold.de', title: 'Its the title of the link' },
+      { name: 'info@lutzweigold.de', to: 'mailto:info@lutzweigold.de', title: 'Its the title of the link' },
+    ],
+  },
+  {
+    title: '© lutz weigold',
+    entries: [
+      { name: 'Imprint', to: '/', title: 'Its the title of the link' },
+      { name: 'info@lutzweigold.de', to: 'mailto:info@lutzweigold.de', title: 'Its the title of the link' },
+    ],
+  },
+];
+</script>
