860995d35b3dbb527284548fb3d5eb48066087ab
Lutz Weigold <dev@lutzweigold.de>
2023-09-09 18:39:28 +0200

    Feat: Add marquee component

---
+++ components/atoms/Marquee.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="cursor-pointer overflow-hidden">
+    <div class="wrapper relative flex text-6xl">
+      <div ref="target" class="box flex">
+        <AtomsTitleText html-tag="span" size="md" class="self-center px-4">{{ props.title }}</AtomsTitleText>
+        <AtomsTitleText html-tag="span" size="md" class="self-center px-4">—</AtomsTitleText>
+        <AtomsTitleText html-tag="span" size="md" class="self-center px-4">{{ props.title }}</AtomsTitleText>
+        <AtomsTitleText html-tag="span" size="md" class="self-center px-4">—</AtomsTitleText>
+        <AtomsTitleText html-tag="span" size="md" class="self-center px-4">{{ props.title }}</AtomsTitleText
+        ><AtomsTitleText html-tag="span" size="md" class="self-center px-4">—</AtomsTitleText>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { $gsap: gsap, $ScrollTrigger: ScrollTrigger } = useNuxtApp();
+
+const target = ref<HTMLDivElement | null>(null);
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  reverse: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+onMounted(() => {
+  const ctx = gsap.context(() => {});
+  ctx.add(() => {
+    let direction = 1;
+
+    const tl = marquee(target.value!, { duration: 30 }, props.reverse || false);
+    ScrollTrigger.create({
+      onUpdate(self) {
+        if (self.direction !== direction) {
+          direction *= -1;
+          gsap.to([tl], { timeScale: direction, overwrite: true });
+        }
+      },
+      markers: false,
+    });
+  });
+});
+
+function marquee(targets: HTMLDivElement | HTMLDivElement[], vars: any, reverse: boolean): gsap.core.Timeline {
+  vars = vars || {};
+  vars.ease || (vars.ease = 'none');
+  const tl: gsap.core.Timeline = gsap.timeline({
+    repeat: -1,
+    onReverseComplete() {
+      this.totalTime(this.rawTime() + this.duration() * 10);
+    },
+  });
+  const elements: Element[] = gsap.utils.toArray(targets);
+  const clones: Element[] = elements.map((el: Element) => {
+    const clone: Element = el.cloneNode(true) as Element;
+    if (el.parentNode) {
+      el.parentNode.appendChild(clone);
+    }
+    return clone;
+  });
+  const positionClones = (): void =>
+    elements.forEach((el: Element, i: number) => {
+      if (el instanceof HTMLElement) {
+        gsap.set(clones[i], {
+          position: 'absolute',
+          overwrite: false,
+          top: el.offsetTop,
+          left: el.offsetLeft + (reverse ? -el.offsetWidth : el.offsetWidth),
+        });
+      }
+    });
+  positionClones();
+  elements.forEach((el: Element, i: number) => tl.to([el, clones[i]], { xPercent: reverse ? 100 : -100, ...vars }, 0));
+  window.addEventListener('resize', () => {
+    const time: number = tl.totalTime();
+    tl.totalTime(0);
+    positionClones();
+    tl.totalTime(time);
+  });
+  return tl;
+}
+</script>
