487d648d591b3c4e147d9176de4ab885c02ba829
Lutz Weigold <dev@lutzweigold.de>
2023-09-18 23:07:15 +0200

    Feat: Add Tag component

---
+++ components/atoms/Tag.vue
@@ -0,0 +1,21 @@
+<template>
+  <AtomsBodyText
+    :class="`p-x-xs inline-block rounded-full border-2 ${
+      props.theme === 'light' ? 'border-black text-black' : 'border-white text-white'
+    }`"
+    ><slot />
+  </AtomsBodyText>
+</template>
+
+<script setup lang="ts">
+import { PropType } from 'vue/dist/vue';
+import { Theme } from '@Types';
+
+const props = defineProps({
+  theme: {
+    type: String as PropType<Theme>,
+    default: 'light',
+    required: false,
+  },
+});
+</script>
