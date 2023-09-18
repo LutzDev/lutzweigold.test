212b30f93a812c4738605d7596eb872a14c50091
Lutz Weigold <dev@lutzweigold.de>
2023-09-18 23:06:08 +0200

    Feat: Add localisation for de-DE and en-US

---
+++ lang/de-DE.ts
@@ -0,0 +1,26 @@
+export default {
+  general: {
+    cta: {
+      menu: 'Menü',
+      language: 'Sprache',
+    },
+  },
+  pages: {
+    about: {
+      title: 'Profil',
+      description: 'Die Bescreibung',
+    },
+    work: {
+      title: 'Projekte',
+      description: 'the description',
+    },
+    skill: {
+      title: 'Skill',
+      description: 'the description',
+    },
+    contact: {
+      title: 'Kontakt',
+      description: 'the description',
+    },
+  },
+};
---
+++ lang/en-US.ts
@@ -0,0 +1,26 @@
+export default {
+  general: {
+    cta: {
+      menu: 'Menu',
+      language: 'Language',
+    },
+  },
+  pages: {
+    about: {
+      title: 'About',
+      description: 'the description',
+    },
+    work: {
+      title: 'Work',
+      description: 'the description',
+    },
+    skill: {
+      title: 'Skill',
+      description: 'the description',
+    },
+    contact: {
+      title: 'Contact',
+      description: 'the description',
+    },
+  },
+};
