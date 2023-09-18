6833d49358fef0161a7aec7dae95a0d2d5317d12
Lutz Weigold <dev@lutzweigold.de>
2023-09-18 23:03:27 +0200

    Config: Add i18n plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,62 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: true,
-  modules: ['@nuxtjs/eslint-module', '@nuxtjs/tailwindcss', '@vueuse/nuxt', '@nuxt/image', 'nuxt-icon', '@pinia/nuxt'],
+  modules: [
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/tailwindcss',
+    '@vueuse/nuxt',
+    '@nuxt/image',
+    'nuxt-icon',
+    '@nuxtjs/i18n',
+    '@pinia/nuxt',
+  ],
   css: ['@/assets/css/main.css'],
+
+  /*  routeRules: {
+    '/': { redirect: '/en-CA' },
+  }, */
+
+  i18n: {
+    customRoutes: 'config',
+    pages: {
+      work: {
+        en: '/work',
+        de: '/projekte',
+      },
+      skill: {
+        en: '/skill',
+        de: '/skill',
+      },
+      contact: {
+        en: '/contact',
+        de: '/kontakt',
+      },
+      /*      'projects/[slug]': {
+        en: 'projects/[slug]',
+        de: 'projekte/[slug]',
+      }, */
+    },
+    locales: [
+      {
+        code: 'en',
+        name: 'English',
+        iso: 'en-US',
+        file: 'en-US.ts',
+      },
+      {
+        code: 'de',
+        name: 'German',
+        iso: 'de-DE',
+        file: 'de-DE.ts',
+      },
+    ],
+    defaultLocale: 'de',
+    lazy: true,
+    strategy: 'prefix',
+    langDir: 'lang',
+    vueI18n: './i18n.config.ts',
+  },
+
   pinia: {
     autoImports: ['storeToRefs', 'defineStore', ['defineStore', 'definePiniaStore']],
   },
