b26aab042b1f6a0d3f749f7120425aa678574484
Lutz Weigold <dev@lutzweigold.de>
2023-09-21 09:25:05 +0200

    Fix: Fix additionalYear poisition on mobile devices

--- components/atoms/ExperienceItem.vue
+++ components/atoms/ExperienceItem.vue
@@ -1,15 +1,22 @@
 <template>
   <AtomsGrid :cols="8" y-gap="sm" class="w-full" @mouseenter="isHover = true" @mouseleave="isHover = false">
-    <div class="col-span-6 md:col-span-2">
+    <div class="col-span-6 flex items-baseline gap-2 md:col-span-2">
       <AtomsTitleText size="lg" class="inline-block"
         ><span class="md:hidden">{{ props.year }}</span
         >{{ props.item.year }}</AtomsTitleText
       >
+      <AtomsSmallText
+        class="inline-block font-medium uppercase tracking-wide text-gray-500 md:hidden"
+        html-tag="span"
+        >{{ props.item.additionalYear }}</AtomsSmallText
+      >
     </div>
     <div class="col-span-full self-end sm:col-span-6 md:col-span-4">
-      <AtomsSmallText class="mb-0.5 inline-block font-medium uppercase tracking-wide text-gray-500" html-tag="span">{{
-        props.item.additionalYear
-      }}</AtomsSmallText>
+      <AtomsSmallText
+        class="mb-1 inline-block hidden font-medium uppercase tracking-wide text-gray-500 md:block"
+        html-tag="span"
+        >{{ props.item.additionalYear }}</AtomsSmallText
+      >
       <AtomsHeadline html-tag="h4" class="mb-0"> {{ props.item.title }}</AtomsHeadline>
       <AtomsGrid v-if="Array.isArray(props.item?.description)" :cols="4" y-gap="none">
         <AtomsBodyText v-for="(entry, key) in props.item?.description" :key="key" class="col-span-2"
@@ -29,8 +36,11 @@
 </template>
 
 <script setup lang="ts">
+import { breakpointsTailwind } from '@vueuse/core';
 import { PropType } from 'vue/dist/vue';
 import { ExperienceItem } from '@Types';
+
+const breakpoints = useBreakpoints(breakpointsTailwind);
 
 const isHover = ref<boolean>(false);
 
