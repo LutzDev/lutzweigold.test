02db83495d11ec50a8d2485866f22d8a1e16623e
Lutz Weigold <dev@lutzweigold.de>
2023-09-23 17:34:16 +0200

    Feat: Add InformationTextItem component

---
+++ components/atoms/InformationTextItem.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="space-y-sm">
+    <slot name="headline" />
+    <div class="space-y-md">
+      <slot name="text" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
