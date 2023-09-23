c6094d067a5a2d875e7b3f2a0c89daad1d8107e9
Lutz Weigold <dev@lutzweigold.de>
2023-09-23 17:44:06 +0200

    Fix: Fix layout break of Portfolio component on mobile devices

--- components/atoms/PortfolioItem.vue
+++ components/atoms/PortfolioItem.vue
@@ -6,11 +6,16 @@
       @mouseenter="isHover = true"
       @mouseleave="isHover = false"
     >
-      <AtomsFlex class="col-span-5 items-baseline" x-gap="sm">
+      <AtomsFlex class="col-span-8 row-start-1 items-baseline xs:col-span-5" x-gap="sm">
         <AtomsBodyText html-tag="span">0{{ props.position }}</AtomsBodyText>
         <AtomsTitleText size="md">{{ props.item.title }}</AtomsTitleText>
       </AtomsFlex>
-      <AtomsFlex class="col-span-3 col-start-6" x-gap="sm" wrap y-gap="sm">
+      <AtomsFlex
+        class="col-span-full col-start-1 row-start-2 xs:col-span-3 xs:col-start-6 xs:row-start-1"
+        x-gap="sm"
+        wrap
+        y-gap="sm"
+      >
         <AtomsTag v-for="(tag, key) in props.item?.tags" :key="key">{{ tag }}</AtomsTag>
       </AtomsFlex>
       <AtomsButton class="col-span-2 col-start-9 justify-self-end" :event="isHover" :to="item.to" size="md" />
