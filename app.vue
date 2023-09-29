72682492d28b9c85d897f054c83fe9d9a92fa501
Lutz Weigold <dev@lutzweigold.de>
2023-09-29 14:13:29 +0200

    Feat: Add overflow-x-hidden to wrapper container

--- app.vue
+++ app.vue
@@ -1,17 +1,19 @@
 <template>
-  <!--  <OrganismsLoadingScreen v-if="false" />-->
-  <div id="smooth-wrapper" class="grid min-h-screen grid-cols-12">
-    <OrganismsHeader class="fixed -z-10 bg-red-600" />
-    <OrganismsAside class="pointer-events-none fixed left-0 right-0 top-0 z-10" />
-    <div id="smooth-content" class="pointer-events-none col-span-full col-start-1 lg:col-span-10 lg:col-start-3">
-      <NuxtLayout>
-        <NuxtPage />
-      </NuxtLayout>
+  <div class="relative overflow-x-hidden">
+    <!--  <OrganismsLoadingScreen v-if="false" />-->
+    <div id="smooth-wrapper" class="grid min-h-screen grid-cols-12">
+      <OrganismsHeader class="fixed -z-10 bg-red-600" />
+      <OrganismsAside class="pointer-events-none fixed left-0 right-0 top-0 z-10" />
+      <div id="smooth-content" class="pointer-events-none col-span-full col-start-1 lg:col-span-10 lg:col-start-3">
+        <NuxtLayout>
+          <NuxtPage />
+        </NuxtLayout>
+      </div>
     </div>
-  </div>
-  <!--  <div class="fixed -z-10">
+    <!--  <div class="fixed -z-10">
     <MoleculesLiquidBackground />
   </div>-->
+  </div>
 </template>
 <script lang="ts" setup>
 const route = useRoute();
