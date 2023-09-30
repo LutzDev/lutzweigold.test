1fb6ea4129bb88b90101a995279310d5d2255a26
Lutz Weigold <dev@lutzweigold.de>
2023-09-30 22:56:07 +0200

    Feat: Enable about page

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,4 @@
 <template>
-  <div class="!h-[100svh] h-screen bg-gray-600"><h1>INDEX SITE</h1></div>
-  <div class="!h-[100svh] h-screen bg-gray-200">Second section</div>
-  <div class="!h-[100svh] h-screen bg-gray-600">Third section</div>
-  <div class="!h-[100svh] h-screen bg-gray-200">Fourgh section</div>
-</template>
-<!--
-<template>
   <div ref="scope">
     <AtomsWrapper is-full-screen html-tag="section" y-padding="lg">
       <template #default="{ isFullScreen }">
@@ -238,5 +231,4 @@ onUnmounted(() => {
   ctx.kill();
 });
 </script>
--->
 <script setup lang="ts"></script>
