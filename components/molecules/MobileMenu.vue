fa8f220d0dcb8f3949ca691b9878315ca985e4a4
Lutz Weigold <dev@lutzweigold.de>
2023-10-04 09:38:30 +0200

    Feat: Add safespace to mobilemenu for ipad devices

--- components/molecules/MobileMenu.vue
+++ components/molecules/MobileMenu.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="pb-safe fixed bottom-0 left-0 z-30 w-full">
-    <AtomsWrapper
-      class="text-white"
-      :style="{ backdropFilter: 'blur(8px) brightness(0.4)', '-webkit-backdrop-filter': 'blur(8px) brightness(0.4)' }"
-    >
+  <div
+    class="pb-safe fixed left-0 z-30 w-full pb-0.5"
+    :style="{
+      bottom: 'calc(0px - env(safe-area-inset-bottom))',
+      backdropFilter: 'blur(8px) brightness(0.4)',
+      '-webkit-backdrop-filter': 'blur(8px) brightness(0.4)',
+    }"
+  >
+    <AtomsWrapper class="text-white">
       <div class="flex divide-x-2 divide-white border-2 border-white">
         <div
           class="pointer-events-auto flex w-1/2 cursor-pointer justify-between px-2 py-1"
